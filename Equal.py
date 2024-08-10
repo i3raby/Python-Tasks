725e07eb92138f05e57996776cc4cd6b6019c7f9
Muhammed Bin Al-Arabi <i3rabyx@gmail.com>
2024-08-10 14:23:39 +0300

    Add files via upload

---
+++ Equal.py
@@ -0,0 +1,35 @@
+Ogryzek = 'Ogryzek'
+Eternxlkz = 'Ogryzek'
+
+print(Ogryzek != Eternxlkz)
+
+if(Ogryzek != Eternxlkz) :
+    print('✔️'); # True
+else :
+    print('❌'); # False
+
+X = 1;
+Y = 10;
+
+if(X > Y) :
+    print('✔️');
+else :
+    print('❌');
+
+X = 1;
+Y = 2;
+
+if(X <= Y) :
+    print('✔️');
+else :
+    print('❌');
+
+
+print(15 + 30); # Result → 45
+print(15 - 30); # Result → -15
+print(15 * 30); # Result → 450
+print(15 / 30); # Result → 0.5
+
+print(15 %  30); # Result → 0.5
+print(15 ** 30); # Result → 0.5
+print(15 // 30); # Result → 0.5
